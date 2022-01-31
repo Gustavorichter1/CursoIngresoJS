bc783c6cfad85647ec4a85d84fb3539c9f122bbd
Gustavorichter1 <98592480+Gustavorichter1@users.noreply.github.com>
2022-01-31 12:28:55 -0300

    commit

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,11 +1,18 @@
 /*
+Gustavo Richter Div E
+e/s ej 3
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	//declaramos la variable
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+
+	//guardamos el nombre
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+
+	//mostrarlo por alert
+	alert("su nombre es " + nombreIngresado);
 
 }
 
